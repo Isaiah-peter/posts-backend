d31d5b1768b01e0f54e9271e2b6b6e72f33352bc
Peter Isaiah <peterisaiah4fun@gmail.com>
2021-09-29 00:24:15 +0100

    nh

--- pkg/config/app.go
+++ pkg/config/app.go
@@ -10,7 +10,7 @@ var (
 )
 
 func Connect() {
-	d, err := gorm.Open("mysql", "b0748306b8e0f7:745801d2@/heroku_abdaf9e81ac8a78?charset=utf8mb4&parseTime=True&loc=Local&reconnect=true")
+	d, err := gorm.Open("mysql", "b0748306b8e0f7:745801d2@tcp(us-cdbr-east-04.cleardb.com:3306)/heroku_abdaf9e81ac8a78?charset=utf8mb4&parseTime=True&loc=Local")
 	if err != nil {
 		panic(err)
 	}
