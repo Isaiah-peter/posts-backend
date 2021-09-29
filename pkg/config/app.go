7ca06a5dbf513c8983075a7a8ad76faa8b923860
Peter Isaiah <peterisaiah4fun@gmail.com>
2021-09-30 00:42:05 +0100

    mor

--- pkg/config/app.go
+++ pkg/config/app.go
@@ -3,6 +3,7 @@ package config
 import (
 	"github.com/jinzhu/gorm"
 	_ "github.com/jinzhu/gorm/dialects/mysql"
+	"os"
 )
 
 var (
@@ -11,7 +12,7 @@ var (
 
 func Connect() {
 	d, err := gorm.Open("mysql", "isaiah:Etanuwoma18@/social?charset=utf8mb4&parseTime=True&loc=Local")
-	c, erre := gorm.Open("mysql", "ba4efbf4b7a5b2:f5e2d1c4@us-cdbr-east-04.cleardb.com/heroku_2f48e549c2f3b08?charset=utf8mb4&parseTime=True&loc=Local")
+	c, erre := gorm.Open("mysql", os.Getenv("CLEARDB_DATABASE_URL"))
   	if err != nil {
 		panic(err)
 	}
