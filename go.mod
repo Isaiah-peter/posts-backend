223b49cd4c3665361ee544fe2f02f260650c914e
Peter Isaiah <peterisaiah4fun@gmail.com>
2021-10-01 01:31:45 +0100

    First commit

--- go.mod
+++ go.mod
@@ -10,6 +10,13 @@ require (
 	github.com/gorilla/mux v1.8.0 // indirect
 	github.com/gorilla/websocket v1.4.2 // indirect
 	github.com/jinzhu/gorm v1.9.16 // indirect
+	github.com/joho/godotenv v1.4.0 // indirect
+	github.com/kr/fs v0.1.0 // indirect
+	github.com/kr/pretty v0.3.0 // indirect
+	github.com/rogpeppe/go-internal v1.8.0 // indirect
 	github.com/rs/cors v1.8.0 // indirect
+	github.com/tools/godep v0.0.0-20180126220526-ce0bfadeb516 // indirect
 	golang.org/x/crypto v0.0.0-20210711020723-a769d52b0f97 // indirect
+	golang.org/x/sys v0.0.0-20210927094055-39ccf1dd6fa6 // indirect
+	golang.org/x/tools v0.1.7 // indirect
 )
--- pkg/config/app.go
+++ pkg/config/app.go
@@ -1,8 +1,10 @@
 package config
 
 import (
+	"fmt"
 	"github.com/jinzhu/gorm"
 	_ "github.com/jinzhu/gorm/dialects/mysql"
+	"github.com/joho/godotenv"
 	"os"
 )
 
@@ -11,18 +13,32 @@ var (
 )
 
 func Connect() {
-	d, err := gorm.Open("mysql", "isaiah:Etanuwoma18@/social?charset=utf8mb4&parseTime=True&loc=Local")
-	c, erre := gorm.Open("mysql", os.Getenv("CLEARDB_DATABASE_URL"))
+    godotenv.Load()
+
+
+	username := os.Getenv("db_user")
+	if username == "" {
+		username = "isaiah"
+	}
+	password := os.Getenv("db_pass")
+	if password == "" {
+		password = "Etanuwoma18"
+	}
+	dbName := os.Getenv("db_name")
+	if dbName== "" {
+		dbName = "social"
+	}
+	dbHost := os.Getenv("db_host")
+	if dbHost == "" {
+		dbHost = "127.0.0.1:3306"
+	}
+	dbUri := fmt.Sprintf("%s:%s@tcp(%s)/%s?charset=utf8&parseTime=True",username ,password ,dbHost, dbName ) //Build connection string
+	fmt.Println(dbUri)
+	d, err := gorm.Open("mysql", dbUri)
   	if err != nil {
 		panic(err)
 	}
-	if erre != nil {
-		panic(erre)
-	}
-	if c == nil {
-		db = d
-	}
-	db = c
+	db = d
 }
 
 func GetDB() *gorm.DB {
